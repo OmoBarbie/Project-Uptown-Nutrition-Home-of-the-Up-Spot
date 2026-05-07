ce865f26bc21c46c06d4ffac39f2b07611919f95
oluwasetemi ojo <setemiojo@gmail.com>
2026-05-07 11:32:15 -0500

    fix(admin): remove nonexistent authClient.Provider, use fragment

--- admin/components/SessionProvider.tsx
+++ admin/components/SessionProvider.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import { authClient } from '@/lib/auth-client';
-
 export function SessionProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <authClient.Provider>
-      {children}
-    </authClient.Provider>
-  );
+  return <>{children}</>
 }
